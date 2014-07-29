202957eedb9a4029a86d923c93a28c595832108c
Shobhit Dutia <shobhitdutia@gmail.com>
2014-07-29 02:45:10 -0400

    Stable

--- LabApplication/src/com/example/labapplicationapk/Server.java
+++ LabApplication/src/com/example/labapplicationapk/Server.java
@@ -3,10 +3,5 @@ public class Server {
 	public static void main(String[] args) {
 		ControllerServer cs=new ControllerServer();
 		cs.init();
-<<<<<<< HEAD
-		System.out.println("Hello");
-=======
-		System.out.println("Testing push v2");
->>>>>>> origin/master
 	}
 }	
